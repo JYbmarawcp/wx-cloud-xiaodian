991dba6784dda09964a8498767a595371c31533d
JYbmarawcp <fjh2333@gmail.com>
2022-03-28 23:26:39 +0800

    coupon

--- miniprogram/pages/coupons/index.js
+++ miniprogram/pages/coupons/index.js
@@ -16,17 +16,35 @@ Page({
       }
     ],
     currentIndex: 0,
-
+    couponsList: []
   },
   onLoad: function (options) {
-
+    wx.cloud.database().collection('user_coupons').where({
+      _openid: app.globalData.openid
+    }).get().then(res => {
+      const couponsList = res.data;
+      couponsList.forEach(item => {
+        item.startTime = this.getTime(item._createTime);
+        item.endTime = this.getTime(item.endTime);
+      })
+      this.setData({
+        couponsList
+      })
+    })
   },
   onShow: function () {
 
   },
   changeTabs(e) {
     this.setData({
-      currentIndex: Number(e.target.dataset.index)
+      currentIndex: Number(e.currentTarget.dataset.index)
     })
-  }
+  },
+  getTime(timestamp) {
+  　　let date = new Date(timestamp);
+  　　var YY = date.getFullYear() + '-';
+  　　var MM = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
+  　　var DD = (date.getDate() < 10 ? '0' + (date.getDate()) : date.getDate());
+  　　return YY + MM + DD;
+  },
 })
