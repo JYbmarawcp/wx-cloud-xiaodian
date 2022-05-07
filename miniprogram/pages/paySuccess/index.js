8d54c90d4596ffa9be309291d23708ccda6a1e40
傅佳杭 <jhfu1@leqee.com>
2022-05-07 16:26:30 +0800

    3版本

--- miniprogram/pages/paySuccess/index.js
+++ miniprogram/pages/paySuccess/index.js
@@ -1,10 +1,21 @@
 // pages/paySuccess/index.js
+const app = getApp()
 Page({
   data: {
     order: {},
-    price: ""
+    price: "",
+    isShow: 0
   },
   onLoad: function (options) {
+    wx.cloud.database().collection('user_coupons').where({
+      _openid: app.globalData.openid,
+      coupon_id: '6d85a2b96275e78e0224d552673b6458',
+      status: 0
+    }).get().then(res => {
+      this.setData({
+        isShow: res.data.length
+      })
+    })
     if (options.order_id) {
       wx.cloud.database().collection('orders').where({
         _id: options.order_id
@@ -46,6 +57,11 @@ Page({
   goToCoupons() {
     wx.navigateTo({
       url: '/pages/coupons/index',
+    })
+  },
+  getCoupon() {
+    wx.navigateTo({
+      url: '/pages/couponDetail/index?id=6d85a2b96275e78e0224d552673b6458',
     })
   }
 })
